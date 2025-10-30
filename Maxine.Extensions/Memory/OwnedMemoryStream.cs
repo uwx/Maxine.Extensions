d4d3d3c8d486d00aedf85f9a14b7d3617eab25e4
uwx <maxine@cybergal.com>
2025-10-30 21:56:51 +0100

    Add OwnedMemoryStream

---
+++ Maxine.Extensions/Memory/OwnedMemoryStream.cs
@@ -0,0 +1,32 @@
+﻿using System.Buffers;
+
+namespace Maxine.Extensions;
+
+public sealed class OwnedMemoryStream : UnmanagedMemoryStream
+{
+    private readonly bool _leaveOpen;
+    private IMemoryOwner<byte>? _owner;
+    private MemoryHandle _handle;
+    public ReadOnlySpan<byte> Span => _owner.Memory.Span[..(int)Position];
+
+    public unsafe OwnedMemoryStream(IMemoryOwner<byte> owner, bool leaveOpen = false)
+    {
+        _leaveOpen = leaveOpen;
+        _owner = owner;
+        _handle = _owner.Memory.Pin();
+        Initialize((byte*)_handle.Pointer, 0L, _owner.Memory.Length, FileAccess.ReadWrite);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        _handle.Dispose();
+        
+        if (!_leaveOpen)
+        {
+            _owner?.Dispose();
+            _owner = null;
+        }
+
+        base.Dispose(disposing);
+    }
+}
