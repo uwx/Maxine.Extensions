61f35bdeca506cae147fb7989643328388b5437e
uwx <maxine@cybergal.com>
2025-10-30 22:37:43 +0100

    Format

--- Maxine.Extensions.Test/StartupTaskTests.cs
+++ Maxine.Extensions.Test/StartupTaskTests.cs
@@ -6,44 +6,43 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Maxine.Extensions.Test
+namespace Maxine.Extensions.Test;
+
+[TestClass]
+public class StartupTaskTests
 {
-    [TestClass]
-    public class StartupTaskTests
+    [TestMethod]
+    public void TestAddStartupTask()
     {
-        [TestMethod]
-        public void TestAddStartupTask()
+        var services = new ServiceCollection();
+        services.AddStartupTask<SampleStartupTask>();
+
+        var provider = services.BuildServiceProvider();
+        var task = provider.GetService<IStartupTask>();
+
+        Assert.IsNotNull(task);
+        Assert.IsInstanceOfType(task, typeof(SampleStartupTask));
+    }
+
+    [TestMethod]
+    public async Task TestExecuteAsync()
+    {
+        var task = new SampleStartupTask();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        await task.ExecuteAsync(cancellationTokenSource.Token);
+
+        Assert.IsTrue(task.Executed);
+    }
+
+    private class SampleStartupTask : IStartupTask
+    {
+        public bool Executed { get; private set; }
+
+        public Task ExecuteAsync(CancellationToken cancellationToken = default)
         {
-            var services = new ServiceCollection();
-            services.AddStartupTask<SampleStartupTask>();
-
-            var provider = services.BuildServiceProvider();
-            var task = provider.GetService<IStartupTask>();
-
-            Assert.IsNotNull(task);
-            Assert.IsInstanceOfType(task, typeof(SampleStartupTask));
-        }
-
-        [TestMethod]
-        public async Task TestExecuteAsync()
-        {
-            var task = new SampleStartupTask();
-            var cancellationTokenSource = new CancellationTokenSource();
-
-            await task.ExecuteAsync(cancellationTokenSource.Token);
-
-            Assert.IsTrue(task.Executed);
-        }
-
-        private class SampleStartupTask : IStartupTask
-        {
-            public bool Executed { get; private set; }
-
-            public Task ExecuteAsync(CancellationToken cancellationToken = default)
-            {
-                Executed = true;
-                return Task.CompletedTask;
-            }
+            Executed = true;
+            return Task.CompletedTask;
         }
     }
 }
