210508e4064fba7780b06b1dc4f764788aa4eee2
uwx <maxine@cybergal.com>
2026-01-11 00:24:00 +0100

    DictionarySlim GetValueOrDefault

---
+++ Maxine.Extensions/Extensions/DictionarySlimExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Collections.Extensions;
+
+namespace Maxine.Extensions;
+
+public static class DictionarySlimExtensions
+{
+    public static TElement? GetValueOrDefault<TKey, TElement>(this DictionarySlim<TKey, TElement> dictionary, TKey key)
+        where TKey : IEquatable<TKey>
+    {
+        return dictionary.TryGetValue(key, out var value) ? value : default!;
+    }
+}
