9f92480c6775704e03756c26f1585206cfe7c66d
uwx <maxine@cybergal.com>
2026-01-24 16:53:51 +0100

    WIP: V2 handwritten generator

--- LuaVisibleAttribute.cs
+++ LuaVisibleAttribute.cs
@@ -22,8 +22,29 @@ public sealed class LuaHiddenAttribute : Attribute;
 /// <summary>
 /// Marks a method or property with a custom Lua name.
 /// </summary>
-[AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field)]
 public sealed class LuaNameAttribute(string name) : Attribute
 {
     public string Name { get; } = name;
+}
+
+/// <summary>
+/// Marks an assembly to indicate that type T should be considered Lua visible.
+/// </summary>
+/// <typeparam name="T">The type.</typeparam>
+[AttributeUsage(AttributeTargets.Assembly)]
+public sealed class AssemblyLuaVisibleAttribute<T>() : AssemblyLuaVisibleAttribute(typeof(T));
+
+/// <summary>
+/// Marks an assembly to indicate that the specified type should be considered Lua visible.
+/// </summary>
+/// <param name="type"></param>
+[AttributeUsage(AttributeTargets.Assembly)]
+public class AssemblyLuaVisibleAttribute(Type type) : Attribute
+{
+    public Type Type { get; } = type;
+    
+    public AssemblyLuaVisibleAttribute(string typeName) : this(Type.GetType(typeName) ?? throw new ArgumentException($"Type '{typeName}' not found."))
+    { 
+    }
 }
