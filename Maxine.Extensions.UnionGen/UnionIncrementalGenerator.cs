f0daad073a904d6dc4624a50a5877e865f571001
uwx <maxine@cybergal.com>
2026-05-30 13:33:28 +0200

    clean up union generator

--- Maxine.Extensions.UnionGen/UnionIncrementalGenerator.cs
+++ Maxine.Extensions.UnionGen/UnionIncrementalGenerator.cs
@@ -82,56 +82,6 @@ public class UnionIncrementalGenerator : IIncrementalGenerator
             sb.AppendLine("{");
             using (sb.Indent())
             {
-                /*
-                [FieldOffset(0)] private readonly sbyte _kind;
-                
-                [FieldOffset(8)] private readonly int _value1;
-                
-                [FieldOffset(8)] private readonly string _value2;
-
-                public IntOrString(int value)
-                {
-                    _kind = 1;
-                    _value1 = value;
-                }
-
-                public IntOrString(string value)
-                {
-                    _kind = 2;
-                    _value2 = value;
-                }
-
-                // still needs to exist for IUnion
-                public object? Value => _kind switch { 1 => _value1, 2 => _value2, _ => null };
-
-                // access pattern that avoids boxing.
-                public bool HasValue => _kind != 0;
-
-                public bool TryGetValue(out int value)
-                {
-                    if (_kind == 1)
-                    {
-                        value = _value1;
-                        return true;
-                    }
-
-                    value = -1;
-                    return false;
-                }
-
-                public bool TryGetValue(out string value)
-                {
-                    if (_kind == 2)
-                    {
-                        value = _value2;
-                        return true;
-                    }
-
-                    value = "";
-                    return false;
-                }
-                 */
-
                 sb.AppendLine("[FieldOffset(0)] private readonly sbyte __union_Kind;");
 
                 if (union.Nullable)
